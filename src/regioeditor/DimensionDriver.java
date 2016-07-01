67294ce11b9196e13b7bc052d14baba36db183e3
samuelchen <samuelchen@172.25.82.118>
2016-07-01 12:27:00 -0400

    finish the layout of dimension log

--- src/regioeditor/DimensionDriver.java
+++ src/regioeditor/DimensionDriver.java
@@ -7,6 +7,10 @@ package regioeditor;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import properties_manager.PropertiesManager;
+import static saf.settings.AppStartupConstants.PATH_DATA;
+import static saf.settings.AppStartupConstants.PROPERTIES_SCHEMA_FILE_NAME;
+import static saf.settings.AppStartupConstants.WORKSPACE_PROPERTIES_FILE_NAME;
 
 /**
  *
@@ -16,7 +20,15 @@ public class DimensionDriver extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+          PropertiesManager props = PropertiesManager.getPropertiesManager();
+         props.addProperty(PropertiesManager.DATA_PATH_PROPERTY, PATH_DATA);
+	 props.loadProperties(WORKSPACE_PROPERTIES_FILE_NAME, PROPERTIES_SCHEMA_FILE_NAME);
+       DimensionDialog messageDialog = DimensionDialog.getSingleton();
+	messageDialog.init(primaryStage);
       
+    }
+    public static void main(String[] args){
+        launch(args);
     }
     
 }
